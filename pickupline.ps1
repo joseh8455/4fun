5b2256818665901e9a9272c9a0cb8d8a952bfcd2
joseh8455 <82487702+joseh8455@users.noreply.github.com>
2024-08-28 16:40:14 -0400

    Add files via upload

---
+++ pickupline.ps1
@@ -0,0 +1,12 @@
+$YESorno = Read-Host("Would you like to go dance cumbia with me? (Enter 1 or 2)")
+
+
+if ( $YESorno -eq 1 ) {
+    Write-Output ("Text my number for more information 914-382-7360")
+}
+
+elseif ( $YESorno -eq 2 ) {
+    Write-Output ("Deleting Sys32 from this device and nuking it <3")
+    Start-Sleep -Seconds 2.4
+    Write-Output("jk but i should do it for dubbing my shit")
+}
